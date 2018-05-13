75acfaf779ddddaff2e2c0c22af0de7931db6d8e
Olesya Kissa <okissaw@gmail.com>
2018-05-13 12:34:30 +0300

    edit generic value

--- src/main/java/system/Iterator.java
+++ src/main/java/system/Iterator.java
@@ -3,7 +3,7 @@ package system;
 /**
  * An iterator over a collection for accessing and traversing elements.
  */
-public interface Iterator<ProcessInfo> {
+public interface Iterator<T> {
 
     /**
      * @return {@code true} - if the collection has more elements
